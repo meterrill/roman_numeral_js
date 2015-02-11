098e9ffefdaee684fbceb6a8a3b38ba50b6400d5
Matthew Terrill <meterrill@gmail.com>
2015-02-11 14:37:56 -0800

    add functionality for hundreds and tens

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,49 @@
 var romanNumeral = function(number) {
-  var thousands = Math.floor(number / 1000);
-  var hundreds = Math.floor((number - (thousands * 1000)) / 100);
-  var numberGroups = [thousands, hundreds, 0, 0];
+  var splitNumber = number.split("");
+  for (var i = splitNumber.length; i < 4; i++) {
+    splitNumber.unshift("0");
+  }
+
+  var thousands = parseInt(splitNumber[0]);
+  var hundreds = parseInt(splitNumber[1]);
+  var tens = parseInt(splitNumber[2]);
+  var ones = parseInt(splitNumber[3]);
   var numerals = ""
 
   for (var i = thousands; i > 0; i--) {
     numerals += "M";
   }
 
-  for (var i = hundreds; i > 0; i--) {
-    numerals += "C";
+  if (hundreds < 4) {
+    for (var i = hundreds; i > 0; i--) {
+      numerals += "C";
+    }
+  } else if (hundreds === 4) {
+    numerals += "CD";
+  } else if (hundreds === 9) {
+    numerals += "CM";
+  } else {
+    numerals += "D";
+    for (var i = hundreds - 5; i > 0; i--) {
+      numerals += "C";
+    }
   }
+
+  if (tens < 4) {
+    for (var i = tens; i > 0; i--) {
+      numerals += "X";
+    }
+  } else if (tens === 4) {
+    numerals += "XL";
+  } else if (tens === 9) {
+    numerals += "XC";
+  } else {
+    numerals += "L";
+    for (var i = tens - 5; i > 0; i--) {
+      numerals += "X";
+    }
+  }
+
 
   return numerals;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,46 @@
 describe("romanNumeral", function() {
-  it("returns M for 1000", function() {
-    expect(romanNumeral(1000)).to.equal("M");
-    expect(romanNumeral(3000)).to.equal("MMM");
+  it("returns correct number of thousands", function() {
+    expect(romanNumeral("1000")).to.equal("M");
   });
 
   it("returns correct number of hundreds", function() {
-    expect(romanNumeral(1300)).to.equal("MCCC");
+    expect(romanNumeral("300")).to.equal("CCC");
   });
+
+  it("returns correct number of hundreds", function() {
+    expect(romanNumeral("400")).to.equal("CD");
+  });
+
+  it("returns correct number of hundreds", function() {
+    expect(romanNumeral("700")).to.equal("DCC");
+  });
+
+  it("returns correct number of hundreds", function() {
+    expect(romanNumeral("900")).to.equal("CM");
+  });
+
+  it("returns correct number of hundreds", function() {
+    expect(romanNumeral("500")).to.equal("D");
+  });
+
+  it("returns correct number of tens", function() {
+    expect(romanNumeral("10")).to.equal("X");
+  });
+
+  it("returns correct number of tens", function() {
+    expect(romanNumeral("40")).to.equal("XL");
+  });
+
+  it("returns correct number of tens", function() {
+    expect(romanNumeral("70")).to.equal("LXX");
+  });
+
+  it("returns correct number of tens", function() {
+    expect(romanNumeral("90")).to.equal("XC");
+  });
+
+  it("returns correct number of tens", function() {
+    expect(romanNumeral("50")).to.equal("L");
+  });
+
 });
